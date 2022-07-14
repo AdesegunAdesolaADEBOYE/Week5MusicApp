712af6318367f01eb57863b484e215b47576dae5
Adesegun Adesola ADEBOYE <fantasticinkmedia@gmail.com>
2022-07-14 18:16:23 +0100

    Updated musicapp/settings.py

--- musicapp/settings.py
+++ musicapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'music_api',
     'authentication',
     'users',
