a0918686bd49f18f435061eecd5d9f4eb97ab4d0
Adesegun Adesola ADEBOYE <fantasticinkmedia@gmail.com>
2022-08-04 14:56:55 +0100

    Installed documentation - Swagger and ReDoc

--- musicapp/urls.py
+++ musicapp/urls.py
@@ -21,8 +21,26 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="MusicApp API Python Portfolio Team 2",
+        default_version='v1',
+        description="Side Hustle 5.0 Portfolio Bootcamp - Week 5 Task",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include ('music_api.urls', namespace='music_api')),
     path('api/user/', include ('users.urls', namespace='users')),
